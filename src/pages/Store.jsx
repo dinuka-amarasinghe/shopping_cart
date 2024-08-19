997f113bfad98a488d73cda7e97979512a93bfb7
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-19 20:54:16 +0530

    add products array with function to fetch product details by ID.

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,6 +1,19 @@
+import {Row, Col} from 'react-bootstrap';
+import {productsArray} from "../productsStore.js";
+
 function Store() {
     return (
-        <h1>Welcome to the Store</h1>
+        <>
+            <h1>Welcome to the Store</h1>
+            <Row xs={1} md={3} className="g-4">
+                <Col align="center">
+                    <h1>Product</h1>
+                </Col>
+                <Col align="center">
+                    <h1>Product</h1>
+                </Col>
+            </Row>
+        </>
     );
 }
 
