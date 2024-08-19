0b7c56871bc8190aad3c22e7b3aa13028b72bc42
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-19 23:57:16 +0530

    add ProductCard component with cart management functionality, conditional rendering

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,14 +6,29 @@ function ProductCard(props) {
     const product = props.product;
     const cart = useContext(CartContext);
     const productQuantity = cart.getProductQuantity(product.id);
-    console.log(cart.items);
-    
+
     return (
         <Card>
             <Card.Body>
                 <Card.Title>{product.title}</Card.Title>
-                <Card.Title>{product.price}</Card.Title>
-                <Button variant="primary" onClick={() => cart.addOneToCart(product.id)}>Add To Cart</Button>
+                <Card.Text>{product.price}</Card.Text>
+                {productQuantity > 0 ?
+                    <>
+                        <Form as={Row}>
+                            <Form.Label column="true" sm="6">In Cart: {productQuantity}</Form.Label>
+                            <Col sm="6">
+                                <Button onClick={() => cart.addOneToCart(product.id)} sm="6" className="mx-2">+</Button>
+                                <Button onClick={() => cart.removeOneFromCart(product.id)} sm="6"
+                                        className="mx-2">-</Button>
+                            </Col>
+                        </Form>
+                        <Button variant="danger" className="my-2" onClick={() => cart.deleteFromCart(product.id)}>Remove
+                            From
+                            Cart</Button>
+                    </>
+                    :
+                    <Button variant="primary" onClick={() => cart.addOneToCart(product.id)}>Add To Cart</Button>
+                }
             </Card.Body>
         </Card>
     );
