03bf194f301797f27f26edca54ada0044410d015
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-19 20:40:38 +0530

    add initial store,success and cancel components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from 'react-bootstrap'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import NavbarComponent from "./components/Navbar.jsx";
+import Store from "./pages/Store.jsx";
+import Success from "./pages/Success.jsx";
+import Cancel from "./pages/Cancel.jsx";
 
 function App() {
-
     return (
         <Container>
             <NavbarComponent/>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Store/>}/>
+                    <Route path="success" element={<Success/>}/>
+                    <Route path="cancel" element={<Cancel/>}/>
+                </Routes>
+            </BrowserRouter>
         </Container>
     )
 }
