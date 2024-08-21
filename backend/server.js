55a6f65dfc6e6f5be3140b7b582e0a6799dc1b46
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-21 19:06:28 +0530

    initialise backend for handling stripe connection

---
+++ backend/server.js
@@ -0,0 +1,4 @@
+// sk_test_51PqERbP5yaa259l55uIBP0I4qsndE0KbyWMaxFJ6gXUEHBUsyJSM4Y732qrahUX4z36OBzn91USOimyEXmiaWm0n00wZ5re3dH
+// Coffee: price_1PqEXLP5yaa259l5MABeOCRy
+// Sunglasses: price_1PqEYRP5yaa259l58BGW6j2h
+// Camera: price_1PqEZdP5yaa259l5ITwRVTCS
